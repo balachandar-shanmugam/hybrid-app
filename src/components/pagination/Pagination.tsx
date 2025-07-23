0e6bdbd07010d00ec61e35cc695819a6464347ef
balachandar-shanmugam <balachandar.s@lloydsbanking.com>
2025-07-23 14:14:42 +0530

    pagination amendment ellipsis changes

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,34 +1,59 @@
 import React, {ReactElement} from 'react';
 import './Pagination.css';
-import { Button } from '@constellation/core';
+import { Button, Icon } from '@constellation/core';
 
 
 function Pagination({ currentPage, totalPages, onPageChange }): ReactElement {
 
-  const handlePrev = () => {
-    if (currentPage > 1) onPageChange(currentPage - 1);
-  };
+const getPageNumbers = () => {
+    const delta = 2;
+    const range = [];
+    const rangeWithDots = [];
+    let l: number;
 
-  const handleNext = () => {
-    if (currentPage < totalPages) onPageChange(currentPage + 1);
-  };
+    for (let i = 1; i <= totalPages; i++) {
+      if (i === 1 || i === totalPages || (i >= currentPage - delta && i <= currentPage + delta)) {
+        range.push(i);
+      }
+    }
+
+    for (let i of range) {
+      if (l) {
+        if (i - l === 2) {
+          rangeWithDots.push(l + 1);
+        } else if (i - l !== 1) {
+          rangeWithDots.push('...');
+        }
+      }
+      rangeWithDots.push(i);
+      l = i;
+    }
+	
+return rangeWithDots;
+
+}
+
 
   return (
     <div id="pagination"  className="pagination-container">
-      <Button onClick={handlePrev} disabled={currentPage === 1}>
-        Previous
+      <Button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+        <Icon name= "chevron-left" />
       </Button>
-      {Array.from({ length: totalPages }, (_, i) => (
-        <Button variation='secondary'
-          key={i}
-          onClick={() => onPageChange(i + 1)}
-          className={currentPage === i + 1 ? 'active' : ''}
-        >
-          {i + 1}
-        </Button>
-      ))}
-      <Button onClick={handleNext} disabled={currentPage === totalPages}>
-        Next
+		{getPageNumbers().map((page, index) =>
+		page === '...' ? (
+			<span key={index} className="dots">...</span>
+		) : (
+			<button
+			key={index}
+			onClick={() => onPageChange(Number(page))}
+			className={currentPage === page ? 'active' : ''}
+			>
+			{page}
+			</button>
+		)
+		)}
+      <Button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+       <Icon name= "chevron-right" />
       </Button>
     </div>
   );
